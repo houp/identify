253d416a8c864852ed6b9278bb02ed611098b37d
Witold Bołt <witold.bolt@hope.art.pl>
2018-06-10 00:15:59 +0200

    Fix for Linux

--- build_lib_gcc.sh
+++ build_lib_gcc.sh
@@ -9,4 +9,4 @@ else
     ext='dll'
 fi
 
-gcc spatial.c tools.c -o python/libspatial.$ext -shared -lgsl -std=gnu99 -DLOG -fopenmp -Wall -O3 -march=native -D_BUILD_LIB
+gcc spatial.c tools.c -o python/libspatial.$ext -shared -lgsl -std=gnu99 -DLOG -fopenmp -Wall -O3 -fPIC -march=native -D_BUILD_LIB -D_GNU_SOURCE
