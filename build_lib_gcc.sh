2d470b450e1a6240d3ce9e1f5b0e535f668488e5
Witold Bołt <witold.bolt@hope.art.pl>
2018-06-10 00:18:33 +0200

    Small update for Linux

--- build_lib_gcc.sh
+++ build_lib_gcc.sh
@@ -10,3 +10,4 @@ else
 fi
 
 gcc spatial.c tools.c -o python/libspatial.$ext -shared -lgsl -std=gnu99 -DLOG -fopenmp -Wall -O3 -fPIC -march=native -D_BUILD_LIB -D_GNU_SOURCE
+strip python/libspatial.$ext
